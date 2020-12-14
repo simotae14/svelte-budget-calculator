dfb2b9d78bd1ffed01fad390f29831974c7f11bc
Simone <startae14@gmail.com>
2020-12-15 00:25:04 +0100

    5. created a new Navbar component and import it into the script file

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,7 @@
-<!-- <script></script> -->
-<!-- JAVASCRIPT LOGIC -->
-
+<script>
+import Navbar from './Navbar';
+</script>
 <!-- <style></style> -->
 <!-- CSS/STYLING -->
 
-<!-- HTML -->
-<nav class="nav">
-	<div class="nav-center">
-		<h1 class="nav-title">
-			budget calculator
-		</h1>
-		<button type="button" class="nav-btn">
-			<i class="far fa-plus-square"></i>
-			add item
-		</button>
-	</div>
-</nav>
+<Navbar />
---
+++ src/Navbar.svelte
@@ -0,0 +1,12 @@
+<!-- HTML -->
+<nav class="nav">
+	<div class="nav-center">
+		<h1 class="nav-title">
+			budget calculator
+		</h1>
+		<button type="button" class="nav-btn">
+			<i class="far fa-plus-square"></i>
+			add item
+		</button>
+	</div>
+</nav>
